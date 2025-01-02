5553ed587c5c893c9ae378dada4e098478fe42a6
Yeon <yennie37@naver.com>
2025-01-02 23:21:56 +0900

    TEST COMMIT_250102

--- src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalogservice/CatalogServiceApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CatalogServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogServiceApplication.class, args);
+		System.out.println("! Main is Running !");
 	}
 }
